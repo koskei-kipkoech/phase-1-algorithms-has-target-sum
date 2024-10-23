1fed2062cdb42949e0328c176eff34e2e56cebe0
koskei-kipkoech <80827464+koskei-kipkoech@users.noreply.github.com>
2024-10-23 14:42:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const numbers = {};
+  for(let num of array){
+    const complete = target - num;
+    if(numbers[complete]){
+      return true
+    }
+    numbers[num] = true 
+  }
+  return false;
 }
-
+console.log(hasTargetSum([9,8,12,4,11,7],20))
 /* 
   Write the Big O time complexity of your function here
+  time complexity O(n) where n is the number of elements in the array. we make a single pass throughout the array O(1)
+  space complexity O(n)in the worst case we may store every number in the array.
 */
 
 /* 
   Add your pseudocode here
+  create an empty object called numbers 
+  for each number in a array ;
+    calculate the complement(target -current number)
+    if complement exist in numbers 
+      return true
+    otheriwse 
+      add the current number to numbers 
+  return false
 */
 
 /*
